2de306524bb91dca4485aa88c811f39766505b82
AssKickerSurfaceBook <121584565@163.com>
2017-12-13 19:54:20 +0800

    able to host static resource

--- webServer.py
+++ webServer.py
@@ -5,42 +5,9 @@ from Network import TextGraph
 import json
 
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    path = environ['PATH_INFO']
-    method = environ['REQUEST_METHOD']
-    if method == 'GET' and path == '/':
-        body = '''<h6>enter your sentence</h6>
-                <form action="/tokenization" method="post">
-                <input name="sentence">
-                <button type="submit">Parse</button> 
-                  </form>'''
-        return [body.encode('utf-8')]
-    if method == "POST" and path == "/tokenization":
-        with open("./presentation/WordLink.html", "rb") as f:
-            body = f.read()
-        return [body]
-    if method == 'GET' and path == '/tokenize-result':
-        cg = CorpusGraph()
-        cg.load_from_json()
-        # cg.build_corpus()
-        # cg.save_as_json()
-        # cg.get_sorted_neighbour('一')
-        # print("###############")
-        # for cge in cg.corpus.edges:
-        #     print(cge)
-        # break
-        # print('###', cg.corpus['朝'])
 
-        tg = TextGraph()
-        # sentences = tg.get_sentences(isRandom=False)
-        sentences = ["准许原告肖振明撤回起诉"]
-        tg.build(sentences)
-        tg.fill_edge(cg)
-        res = json.dumps(tg.make_json(cg), ensure_ascii=False)
-        return [res.encode('utf-8')]
-
-
+cg = CorpusGraph()
+cg.load_from_json()
 def content_type(path):
     if path.endswith(".css"):
         return "text/css"
@@ -54,6 +21,16 @@ def content_type(path):
 
 def app(environ, start_response):
     path_info = environ["PATH_INFO"]
+    method = environ['REQUEST_METHOD']
+    if method == 'GET' and path_info == '/tokenize-result':
+        tg = TextGraph()
+        # sentences = tg.get_sentences(isRandom=False)
+        sentences = ["准许原告肖振明撤回起诉"]
+        tg.build(sentences)
+        tg.fill_edge(cg)
+        res = json.dumps(tg.make_json(cg), ensure_ascii=False)
+        return [res.encode('utf-8')]
+
     resource = "WordLink.html"
     if '/' in path_info:
         subroot = path_info.split("/")[1]
